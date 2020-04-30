2defed394116536ee1706637af49291275c15a80
Thu Nguyen <thunguy@me.com>
2020-04-30 14:09:59 -0700

    add slice output

--- list_operations.py
+++ list_operations.py
@@ -9,8 +9,7 @@ def head(input_list):
       >>> head(['Jan', 'Feb', 'Mar'])
       'Jan'
     """
-
-    return None
+    return input_list[0]
 
 
 def tail(input_list):
@@ -22,8 +21,8 @@ def tail(input_list):
     ['Feb', 'Mar']
 
     """
-
-    return []
+    new_list = input_list
+    return new_list[1:]
 
 
 def last(input_list):
@@ -35,8 +34,8 @@ def last(input_list):
     'Mar'
 
     """
-
-    return []
+    # return input_list.pop()
+    return input_list[-1]
 
 
 def top(input_list):
@@ -49,7 +48,7 @@ def top(input_list):
 
     """
 
-    return []
+    return input_list[:-1]
 
 
 def first_three(input_list):
@@ -62,7 +61,7 @@ def first_three(input_list):
 
     """
 
-    return []
+    return input_list[:3]
 
 
 def last_five(input_list):
@@ -75,7 +74,7 @@ def last_five(input_list):
 
     """
 
-    return []
+    return input_list[-5:]
 
 
 def middle(input_list):
@@ -88,7 +87,7 @@ def middle(input_list):
 
     """
 
-    return []
+    return input_list[2:-2]
 
 
 def inner_four(input_list):
@@ -101,7 +100,7 @@ def inner_four(input_list):
 
     """
 
-    return []
+    return input_list[2:6]
 
 
 def inner_four_end(input_list):
@@ -117,7 +116,7 @@ def inner_four_end(input_list):
 
     """
 
-    return []
+    return input_list[-6:-2]
 
 
 def replace_head(input_list):
@@ -131,7 +130,7 @@ def replace_head(input_list):
     True
 
     """
-
+    input_list[0] = 42
     pass
 
 
@@ -147,6 +146,8 @@ def replace_third_and_last(input_list):
 
     """
 
+    input_list[2] = 37
+    input_list[-1] = 37    
     pass
 
 
@@ -165,7 +166,7 @@ def replace_middle(input_list):
     True
 
     """
-
+    input_list[2:-2] = [42, 37]
     pass
 
 
@@ -180,8 +181,8 @@ def delete_third_and_seventh(input_list):
     True
 
     """
-
-    pass
+    del input_list[2]
+    del input_list[5]
 
 
 def delete_middle(input_list):
@@ -197,7 +198,7 @@ def delete_middle(input_list):
     True
 
     """
-
+    input_list[2:-2] = []
     pass
 
 
